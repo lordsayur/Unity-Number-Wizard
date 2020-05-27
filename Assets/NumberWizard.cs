f44bdd426e5748354dbdc36320d2bf69603d45ee
Lord Sayur A <umar_317@hotmail.com>
2020-05-28 07:55:53 +0800

    Create player instruction

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]
+    private int max = 1000;
+    [SerializeField]
+    private int min = 1;
+
+    void Start()
+    {
+        Debug.Log("Selamat datang ke Setau Gaban.");
+        Debug.Log("Cuba pilih numbur lapas tu jangan bagi tau...");
+        Debug.Log($"Numbur paling basar: {max}");
+        Debug.Log($"Numbur paling damit: {min}");
+    }
+}
