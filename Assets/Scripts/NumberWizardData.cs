0a0d124d16600c6c13b6de3ea8e10a3fdb08a0c1
Lord Sayur A <umar_317@hotmail.com>
2020-05-30 09:05:28 +0800

    Create NumberWizardData script

---
+++ Assets/Scripts/NumberWizardData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizardData
+{
+    public int Max { get; set; }
+    public int Min { get; set; }
+    public int OurGuess { get; set; }
+}
